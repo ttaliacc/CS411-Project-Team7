b9d2287149527d02aae19dc110288a7ba23e280c
Tai Koeda <gwckoeda@gmail.com>
2023-04-09 19:39:12 -0400

    simplified

--- project/api/views.py
+++ project/api/views.py
@@ -5,13 +5,10 @@ from rest_framework.views import APIView
 from django.conf import settings
 
 # Create your views here.
-def index(request):
-    return render(request, 'api/index.html')
 
 class SearchResult(APIView):
-    def get(self, request):
+    def get(self, request, query):
         # Get the search query from the 'q' URL parameter.
-        query = request.GET.get('q')
         if query:
             # Call the TMDB API to search for movies with query if it exists.
             response = requests.get(f'https://api.themoviedb.org/3/search/movie?api_key={settings.TMDB_API_KEY}&query={query}')
@@ -20,10 +17,10 @@ class SearchResult(APIView):
             results = response.json()['results']
             for result in results:
                 # Call the Utelly API to get streaming information for each movie.
-                response = requests.get(f'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?country=us&source_id={result["id"]}&source=tmdb', headers={'x-rapidapi-key': settings.UTELLY_API_KEY, 'x-rapidapi-host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'})
+                response = requests.get(f'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?country=us&source_id={result["id"]}&source=tmdb', headers={'x-rapidapi-key': settings.X_RAPIDAPI_KEY, 'x-rapidapi-host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'})
                 
                 # Add the streaming information to the search result dictionary.
-                result['streaming_info'] = response.json()
+                result['streaming_info'] = response.json()['collection']
         else:
             # If no search query was provided, return an error message.
             return Response('Please enter a search query')
@@ -39,8 +36,8 @@ class MovieDetails(APIView):
         movie = response.json()
 
         # Call the Utelly API to get streaming information for the movie
-        response = requests.get(f'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?country=us&source_id={movie_id}&source=tmdb', headers={'x-rapidapi-key': settings.UTELLY_API_KEY, 'x-rapidapi-host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'})
-        streaming_info = response.json()
+        response = requests.get(f'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?country=us&source_id={movie_id}&source=tmdb', headers={'x-rapidapi-key': settings.X_RAPIDAPI_KEY , 'x-rapidapi-host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'})
+        streaming_info = response.json()['collection']
 
         # Return the movie details and streaming information as a JSON response.
         return Response({'movie': movie, 'streaming_info': streaming_info})
