8aabd934bd8c4a3835c9cee0e65a3149520d6e16
Tai Koeda <gwckoeda@gmail.com>
2023-04-09 19:12:58 -0400

    updated to use java rest framework

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
-
+from .views import (
+		SearchResult,
+        MovieDetails
+	)
 urlpatterns = [
     path('', views.index, name='index'),
-    path("search/", views.search, name="search"),
-
+    path("search/", SearchResult.as_view()),
+    path('movie/<int:movie_id>/', MovieDetails.as_view()),
 ]
--- project/api/views.py
+++ project/api/views.py
@@ -1,60 +1,48 @@
 from django.shortcuts import render
 import requests
-from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.conf import settings
 
 # Create your views here.
 def index(request):
     return render(request, 'api/index.html')
 
-def search(request):
-    data = tmdb(request)
-    streaming = {}
-    for movie in data.json()['results']:
-        stream_info = utelly(movie['original_title'])
-        if stream_info['results']:
-            streaming[movie['id']] = stream_info['results'][0]['locations'][0]
-    context = {
-        'data': data.json(),
-        'streaming': streaming,
-        }
-    return render(request, 'api/results.html', context)
+class SearchResult(APIView):
+    def get(self, request):
+        # Get the search query from the 'q' URL parameter.
+        query = request.GET.get('q')
+        if query:
+            # Call the TMDB API to search for movies with query if it exists.
+            response = requests.get(f'https://api.themoviedb.org/3/search/movie?api_key={settings.TMDB_API_KEY}&query={query}')
 
-TMDB_API_KEY = '3016e6c23565d0fd1b4b0e0953da86d2'
-UTELLY_API_URL = url = "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup"
-X_RAPIDAPI_KEY = "45a4c67909msh27c81fcbd07a4fap1a1b86jsn62fd26476f1a"
-X_RAPIDAPI_HOST = "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com"
+            # Get the search results from the TMDB API response.
+            results = response.json()['results']
+            for result in results:
+                # Call the Utelly API to get streaming information for each movie.
+                response = requests.get(f'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?country=us&source_id={result["id"]}&source=tmdb', headers={'x-rapidapi-key': settings.UTELLY_API_KEY, 'x-rapidapi-host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'})
+                
+                # Add the streaming information to the search result dictionary.
+                result['streaming_info'] = response.json()
+        else:
+            # If no search query was provided, return an error message.
+            return Response('Please enter a search query')
+        
+        # Return the search results with streaming information as a JSON response.
+        return Response(results)
+        
 
-def tmdb(request):
-    # Get the user's search query from the request
-    query = request.GET.get('q')
+class MovieDetails(APIView):
+    def get(self, request, movie_id):
+        # Call the TMDB API to get the details for a specific movie
+        response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={settings.TMDB_API_KEY}')
+        movie = response.json()
 
-    # Construct the search request to the API
-    if query:
-        url = 'https://api.themoviedb.org/3/search/movie'
-        params = {
-        'api_key': TMDB_API_KEY,
-        'query': query
-        }  
+        # Call the Utelly API to get streaming information for the movie
+        response = requests.get(f'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?country=us&source_id={movie_id}&source=tmdb', headers={'x-rapidapi-key': settings.UTELLY_API_KEY, 'x-rapidapi-host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'})
+        streaming_info = response.json()
 
-        # Make the request to the API and get the response
-        data = requests.get(url, params=params)
-    else:
-        return HttpResponse("Please enter a search query")
-    # type = request.GET.get('type')
-    return data
+        # Return the movie details and streaming information as a JSON response.
+        return Response({'movie': movie, 'streaming_info': streaming_info})
 
-def utelly(keyword):
-    headers = {
-        "X-RapidAPI-Key": X_RAPIDAPI_KEY,
-        "X-RapidAPI-Host": X_RAPIDAPI_HOST
-    }
-    params = {
-        "term": keyword,
-        "country": "us"
-    }
-    data = requests.get(UTELLY_API_URL, headers=headers, params=params)
-    streaming = data.json()
-
-    return streaming
-    
 
