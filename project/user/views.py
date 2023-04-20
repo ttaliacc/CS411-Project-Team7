ffd7730ae6e2e165f58ee568bbbf15dfd951d0f9
Tai Koeda <gwckoeda@gmail.com>
2023-04-20 16:22:59 -0400

    .

--- project/user/views.py
+++ project/user/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import logout
 from allauth.socialaccount.models import SocialAccount
 def login(request):
     user_email = request.user.email
-    return render(request, 'user/login.html', {'user_email':user_email})
+    return render(request, 'user/login.html')
     
 def logout_view(request):
     logout(request)
