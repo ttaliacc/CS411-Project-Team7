dcadf3b74bb66154258a035c689101669a7b0a66
Qixuan Chen <103902059+ttaliacc@users.noreply.github.com>
2023-04-20 13:44:43 -0400

    update the url and view for login and about page

--- project/api/urls.py
+++ project/api/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path("search/", views.SearchResult, name="search"),
+    path("signIn/", views.signIn, name="signIn"),
+    path("about/", views.about, name="about"),
 
 ]
