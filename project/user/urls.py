425bad1771989bc809ad34186cbf1e6799e1e9b8
Qixuan Chen <103902059+ttaliacc@users.noreply.github.com>
2023-04-22 21:17:55 -0400

    update the login path

--- project/user/urls.py
+++ project/user/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.login, name='index'),
+    path('login', views.login, name='login'),
     path('logout/', views.logout_view, name='logout'),
 ]
