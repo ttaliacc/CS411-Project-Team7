3a72a01f7ea782b4d5870d576ef10537355c2c16
Tai Koeda <gwckoeda@gmail.com>
2023-04-22 20:18:16 -0400

    debug

--- project/backend/settings.py
+++ project/backend/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google', #Google login
 ]
 
-SITE_ID = 4
+SITE_ID = 5
 
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
@@ -169,3 +169,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# {
+#     "web": {
+#         "client_id": "586734677780-vv5o4bao88hcf8q3lnl1bg40bji79b4d.apps.googleusercontent.com",
+#         "project_id": "movie-generator-383804",
+#         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+#         "token_uri": "https://oauth2.googleapis.com/token",
+#         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+#         "client_secret": "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",
+#         "redirect_uris": [
+#             "http://127.0.0.1:8000",
+#             "http://127.0.0.1:8000/accounts/google/login/callback"
+#         ]
+#     }
+# }
