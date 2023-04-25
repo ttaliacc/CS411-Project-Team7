4bbfd674ea860278df078d1c86a845074475fac3
Tai Koeda <gwckoeda@gmail.com>
2023-04-25 13:19:56 -0400

    random word generator and genre filter

--- project/api/views.py
+++ project/api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 import requests
 from django.http import HttpResponse, HttpResponseRedirect
 from django.conf import settings
@@ -7,6 +7,7 @@ from django.core.serializers import serialize
 from allauth.socialaccount.models import SocialAccount
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView
+from random_word import RandomWords
 
 
 #https://github.com/celiao/tmdbsimple
@@ -43,72 +44,76 @@ def SearchResult(request):
     # Get the search query from the 'q' URL parameter.
     query = request.GET.get('q')
     if query:
-        squery = request.GET.get('q')
-    if query:
         url = 'https://api.themoviedb.org/3/search/movie'
         params = {
         'api_key': settings.TMDB_API_KEY,
-        'query': query
+        'query': query,
+        'page': request.GET.get('page', 1)
         }  
         # Call the TMDB API to search for movies with query if it exists.
         response = requests.get(url, params=params)
         results = response.json()['results']
-
+        with_genres = request.GET.get('with_genres', '28,12,16,35,80,99,19751,14,36,27,10402,9648,10749,878,10770,53,10752,37')
+        with_genres = [int(s) for s in with_genres.split(",")]
         # Insert the movies queried into the database
         finalresult = []
         for movie in results:
-            id = movie['id'] 
-            adult = movie['adult']
-            oglanguage = movie['original_language']
-            ogtitle = movie['original_title']
-            overview = movie['overview']
-            title = movie['title']
-            video = movie['video']
-            release = movie['release_date']
-            genres = movie['genre_ids']
-            posterpath = movie['poster_path']
-            backdroppath = movie['backdrop_path']
+            # print(movie['genre_ids'])
+            if any(genre_id in movie['genre_ids'] for genre_id in with_genres):
+                id = movie['id'] 
+                adult = movie['adult']
+                oglanguage = movie['original_language']
+                ogtitle = movie['original_title']
+                overview = movie['overview']
+                title = movie['title']
+                video = movie['video']
+                release = movie['release_date']
+                genres = movie['genre_ids']
+                posterpath = movie['poster_path']
+                backdroppath = movie['backdrop_path']
 
-            ## INSERT INTO Movies
-            if(len(release) != 10) :
-                release = None
-            
-            Movie.objects.get_or_create(id = id,defaults={"poster_path":posterpath, "backdrop_path":backdroppath, "adult" : adult,"release_date":release, "original_language":  oglanguage, "original_title" : ogtitle, "overview":overview, "title":title, "video":video}) 
-            d2 = Movie.objects.get(id=id)
+                ## INSERT INTO Movies
+                if(len(release) != 10) :
+                    release = None
+                
+                Movie.objects.get_or_create(id = id,defaults={"poster_path":posterpath, "backdrop_path":backdroppath, "adult" : adult,"release_date":release, "original_language":  oglanguage, "original_title" : ogtitle, "overview":overview, "title":title, "video":video}) 
+                d2 = Movie.objects.get(id=id)
 
-            
-            ## Assigning each movies its genres 
-            for genre in genres:
-                d2.genres.add(genre)
+                
+                ## Assigning each movies its genres 
+                for genre in genres:
+                    d2.genres.add(genre)
 
 
-            ##print(d2.__dict__) to see fields
-            ##Checking if genres worked
-            ##for genre in d2.genres.all():
-                   ##print(genre.__dict__)
-            finalresult.append(d2)
+                ##print(d2.__dict__) to see fields
+                ##Checking if genres worked
+                ##for genre in d2.genres.all():
+                    ##print(genre.__dict__)
+                finalresult.append(d2)
+
         
         for result in results:
-            # Call the Utelly API to get streaming information for each movie.
-            response = requests.get(f'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?country=us&source_id={result["id"]}&source=tmdb', headers={'x-rapidapi-key': settings.X_RAPIDAPI_KEY, 'x-rapidapi-host': settings.X_RAPIDAPI_HOST})
-            id = result['id']
-            d2 = Movie.objects.get(id=id) 
-            # Add the streaming information to the search result dictionary.
-            result['streaming_info'] = response.json()['collection']
-            if(result['streaming_info']):
-                xd=result['streaming_info']['locations']
-                for x in xd:
-                    displayn = x['display_name']
-                    sid = x['id']
-                    url = x['url']
-                    name = x['name']
-                    icon = x['icon']
-                    ## Inserting the streaming info for each movie
-                    newinfo = StreamInfo.objects.get_or_create(display_name=displayn,sid=sid,url=url,name=name,icon=icon)
-                    d2.streaminfo.add(newinfo[0].id)
-            ##Just some checking
-            ##for genre in d2.streaminfo.all():
-                ##print(genre.__dict__)
+            if any(genre_id in movie['genre_ids'] for genre_id in with_genres):
+                # Call the Utelly API to get streaming information for each movie.
+                response = requests.get(f'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?country=us&source_id={result["id"]}&source=tmdb', headers={'x-rapidapi-key': settings.X_RAPIDAPI_KEY, 'x-rapidapi-host': settings.X_RAPIDAPI_HOST})
+                id = result['id']
+                d2 = Movie.objects.get(id=id) 
+                # Add the streaming information to the search result dictionary.
+                result['streaming_info'] = response.json()['collection']
+                if(result['streaming_info']):
+                    xd=result['streaming_info']['locations']
+                    for x in xd:
+                        displayn = x['display_name']
+                        sid = x['id']
+                        url = x['url']
+                        name = x['name']
+                        icon = x['icon']
+                        ## Inserting the streaming info for each movie
+                        newinfo = StreamInfo.objects.get_or_create(display_name=displayn,sid=sid,url=url,name=name,icon=icon)
+                        d2.streaminfo.add(newinfo[0].id)
+                ##Just some checking
+                ##for genre in d2.streaminfo.all():
+                    ##print(genre.__dict__)
         
 
     else:
@@ -118,7 +123,6 @@ def SearchResult(request):
     return render(request, 'api/results.html', context)
     # Return the search results with streaming information as a JSON response.
 
-#Currently not supported
 def MovieDetails(request, movie_id):
 
     # Get imdb id using TMDB API
@@ -133,41 +137,8 @@ def MovieDetails(request, movie_id):
 
     return redirect(request.path_info)
 
-#Needs Work 
-@login_required
-class FavoriteMovie(ListView):
-    def get_context_data(self, **kwargs):
-            context = super().get_context_data(**kwargs)
-            favorites = FavoritedMovie.objects.filter(user=self.request.user)
-            context['favorites_movie_id'] = [favorite.movie_id for favorite in favorites]
-            return context
-
-    def get_favorite_movies(self):
-        return FavoritedMovie.objects.filter(user=self.request.user)
-
-
-    def add_delete(request, movie_id):
-        favorites = FavoritedMovie.objects.filter(user=request.user, movie_id=movie_id)
-        is_favorited = True if favorites else False
-
-        if request.method == 'POST':
-            if 'add' in request.POST:
-                if not is_favorited:
-                    favorite = FavoritedMovie(user=request.user, movie_id=movie_id)
-                    favorite.save()
-                    return HttpResponseRedirect(request.path_info)
-                else:            
-                    return HttpResponse('Movie already in favorites') 
-            if 'remove' in request.POST:
-                favorites.delete()
-                return HttpResponseRedirect(request.path_info)
-
-
-        context = {
-            'movie_id': movie_id,
-            'is_favorite': is_favorited
-        }
-        return render(request, 'movie_detail.html', context)
-
-
-
+def random_query(request):
+    r = RandomWords()
+    random_query = r.get_random_word()
+    context = {'random_query': random_query}
+    return render(request, 'api/index.html', context)
